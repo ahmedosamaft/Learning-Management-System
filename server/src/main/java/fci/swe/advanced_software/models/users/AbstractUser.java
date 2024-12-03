c6d0ca5343d07a2e7fee81c2abcca706f0798f54
Ahmed Osama <76051700+ahmedosamaft@users.noreply.github.com>
2024-12-04 01:45:00 +0200

    Added: Course to Notification

--- server/src/main/java/fci/swe/advanced_software/models/users/AbstractUser.java
+++ server/src/main/java/fci/swe/advanced_software/models/users/AbstractUser.java
@@ -1,7 +1,11 @@
 package fci.swe.advanced_software.models.users;
 
 import fci.swe.advanced_software.models.AbstractEntity;
+import fci.swe.advanced_software.models.Notification;
 import jakarta.persistence.*;
+
+import java.util.Set;
+import java.util.TreeSet;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -15,4 +19,8 @@ public abstract class AbstractUser extends AbstractEntity {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "recipient")
+    @Column(nullable = false)
+    private Set<Notification> notifications = new TreeSet<>();
 }
