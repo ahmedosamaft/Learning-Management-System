44207e5e9f10215b24c5d6abc5058b66252090ab
Mohamed Abdelhady <mohamedabdelhady.official@gmail.com>
2025-02-14 00:11:16 +0200

    Updated: utils.ts

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getToken() {
-  return 'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyNmM4M2MyZC01MTRlLTQ3ODktYWYxOC1iOGE0OTA1N2IyZDEiLCJyb2xlIjpbIlJPTEVfQURNSU4iXSwiaWF0IjoxNzM5NDgwMDIxLCJleHAiOjE3Mzk0ODM2MjF9.VBKfIEds5OIO94xpoBwT_fbghf_3ZC_3CunfcJKVlAsjzwvjwK_5NV4Zou6gQAmi';
+  return 'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiI3ZDJhZGFiYy1mNjY2LTQ0OTMtYjZlMi04MDVhMjAyZTRkNTEiLCJyb2xlIjpbIlJPTEVfQURNSU4iXSwiaWF0IjoxNzM5NDgzNTQ2LCJleHAiOjE3Mzk0ODcxNDZ9.TUrps4H9WUwkTbIhZabG9YqTnMqMfnjX5uvurjAes1pC6ElvokALIqz2PO0dDLGm';
 }
 
 export function randomInt(min: number, max: number) {
